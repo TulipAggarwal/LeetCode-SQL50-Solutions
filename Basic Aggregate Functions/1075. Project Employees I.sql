da6cb37049b3e34fc1ff3e150e275d9ce880463d
Tulip Aggarwal <93984886+TulipAggarwal@users.noreply.github.com>
2023-05-29 21:19:19 +0530

    Update 1075. Project Employees I.sql

--- Basic Aggregate Functions/1075. Project Employees I.sql
+++ Basic Aggregate Functions/1075. Project Employees I.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
-
 SELECT Project.project_id, ROUND(AVG(experience_years),2) as average_years
 FROM Project JOIN Employee
 ON Project.employee_id = Employee.employee_id
