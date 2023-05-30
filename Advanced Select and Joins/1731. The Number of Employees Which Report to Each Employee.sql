22b55397ba8e065faefbd1a965055a4040737b75
Tulip Aggarwal <93984886+TulipAggarwal@users.noreply.github.com>
2023-05-30 15:28:02 +0530

    Create 1731. The Number of Employees Which Report to Each Employee.sql

---
+++ Advanced Select and Joins/1731. The Number of Employees Which Report to Each Employee.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT a.employee_id, a.name, COUNT(*) AS reports_count, ROUND(AVG(b.age)) AS average_age
+FROM Employees a, Employees b
+WHERE a.employee_id = b.reports_to
+GROUP BY 1
+ORDER BY 1
