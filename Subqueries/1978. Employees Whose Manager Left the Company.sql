35e5e21a0bfb5d49e473d085984d5b66b4a0a488
Tulip Aggarwal <93984886+TulipAggarwal@users.noreply.github.com>
2023-06-12 16:25:06 +0530

    Create 1978. Employees Whose Manager Left the Company.sql

---
+++ Subqueries/1978. Employees Whose Manager Left the Company.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT employee_id
+FROM Employees
+WHERE salary < 30000 AND manager_id IS NOT NULL AND manager_id NOT IN(
+    SELECT employee_id 
+    FROM employees
+)
+ORDER BY employee_id
