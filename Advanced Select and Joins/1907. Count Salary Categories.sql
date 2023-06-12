c8954876db40f66790ad5dfe93f803b39d225eb2
Tulip Aggarwal <93984886+TulipAggarwal@users.noreply.github.com>
2023-06-12 16:14:39 +0530

    Create 1907. Count Salary Categories.sql

---
+++ Advanced Select and Joins/1907. Count Salary Categories.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT "Low Salary" AS Category, SUM(income < 20000) AS accounts_count
+FROM Accounts
+UNION
+SELECT "Average Salary" Category, SUM(income >= 20000 AND income <= 50000) AS accounts_count
+FROM Accounts
+UNION
+SELECT "High Salary" category, SUM(income > 50000) AS accounts_count
+FROM Accounts
