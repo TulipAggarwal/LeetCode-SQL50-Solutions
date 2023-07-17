63659dc7e196fa7ca34ed42f2d5d6e3ae83dc9c2
Tulip Aggarwal <93984886+TulipAggarwal@users.noreply.github.com>
2023-07-17 23:43:42 +0530

    Create 1327. List the Products Ordered in a Period.sql

---
+++ Advanced String Functions,Regex,Clause/1327. List the Products Ordered in a Period.sql
@@ -0,0 +1,5 @@
+SELECT p.product_name AS product_name, sum(o.unit) AS unit FROM Products p
+JOIN Orders o USING (product_id)
+WHERE YEAR(o.order_date)='2020' AND MONTH(o.order_date)='02'
+GROUP BY p.product_id
+HAVING SUM(o.unit)>=100
