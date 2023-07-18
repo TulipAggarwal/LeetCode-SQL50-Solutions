09ffe0e207647ebd443ec1a00b75b597bf0b6f36
Tulip Aggarwal <93984886+TulipAggarwal@users.noreply.github.com>
2023-07-18 16:35:01 +0530

    Create 185. Department Top Three Salaries.sql

---
+++ Subqueries/185. Department Top Three Salaries.sql
@@ -0,0 +1,7 @@
+SELECT d.Name AS Department, e1.Name AS Employee, e1.Salary
+FROM Employee e1 
+JOIN Department d ON e1.departmentId = d.Id
+WHERE 3 > (SELECT COUNT(DISTINCT e2.Salary)
+        FROM Employee e2
+        WHERE e2.Salary > e1.Salary AND e1.DepartmentId = e2.DepartmentId
+        )
